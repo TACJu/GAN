e3e2cb44da161b342044cfe89145a38621554f68
TACJu <1600012870@pku.edu.cn>
2018-11-04 20:48:34 +0800

    add wgan

--- WGAN/WGAN.py
+++ WGAN/WGAN.py
@@ -4,14 +4,15 @@ import os
 
 from keras.layers import Input, Dense, Flatten, Reshape, Conv2D, UpSampling2D, ZeroPadding2D, Dropout, BatchNormalization, Activation
 from keras.layers.advanced_activations import LeakyReLU
-from keras.optimizers import Adam
+from keras.optimizers import RMSprop
 from keras.models import Sequential, Model
 from keras.utils import plot_model
+import keras.backend as K
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 plt.switch_backend('agg')
 
-class DCGAN():
+class WGAN():
     
     def __init__(self):
         self.row = 28
@@ -19,22 +20,27 @@ class DCGAN():
         self.channel = 1
         self.img_shape = (self.row, self.col, self.channel)
         self.latent_dim = 100
+        self.n_critic = 5
+        self.clip_value = 0.01
 
-        optimizer = Adam(0.0002, 0.5)
+        optimizer = RMSprop(lr = 0.00005)
 
-        self.discriminator = self.build_discriminator()
-        self.discriminator.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-        self.discriminator.trainable = False
+        self.critic = self.build_critic()
+        self.critic.compile(loss=self.wasserstein_loss, optimizer=optimizer, metrics=['accuracy'])
+        self.critic.trainable = False
 
         self.generator = self.build_generator()
 
         noise = Input(shape=(self.latent_dim,))
         img = self.generator(noise)
 
-        pred = self.discriminator(img)
+        pred = self.critic(img)
 
         self.combined = Model(noise, pred)
-        self.combined.compile(loss='binary_crossentropy', optimizer=optimizer)
+        self.combined.compile(loss=self.wasserstein_loss, optimizer=optimizer, metrics=['accuracy'])
+
+    def wasserstein_loss(self, label, pred):
+        return K.mean(label * pred)
 
     def build_generator(self):
 
@@ -61,7 +67,7 @@ class DCGAN():
 
         return Model(noise, img)
 
-    def build_discriminator(self):
+    def build_critic(self):
 
         model = Sequential()
 
@@ -82,17 +88,17 @@ class DCGAN():
         model.add(LeakyReLU(alpha=0.2))
         model.add(Dropout(0.25))
         model.add(Flatten())
-        model.add(Dense(1, activation='sigmoid'))
+        model.add(Dense(1))
 
         model.summary()
-        plot_model(model, to_file='discriminator.png')
+        plot_model(model, to_file='critic.png')
 
         img = Input(shape=self.img_shape)
         pred = model(img)
 
         return Model(img, pred)
 
-    def train(self, epochs, batch_size=128, save_interval=50):
+    def train(self, epochs, batch_size=64, save_interval=50):
 
         f = np.load('../mnist.npz')
         X_train = f['x_train']
@@ -101,23 +107,30 @@ class DCGAN():
         X_train = X_train / 127.5 - 1.
         X_train = np.expand_dims(X_train, axis=3)
 
-        real = np.ones((batch_size, 1))
+        real = -np.ones((batch_size, 1))
         fake = np.zeros((batch_size, 1))
 
         for epoch in range(epochs):
-            idx = np.random.randint(0, X_train.shape[0], batch_size)
-            real_imgs = X_train[idx]
+            for i in range(self.n_critic):
+            
+                idx = np.random.randint(0, X_train.shape[0], batch_size)
+                real_imgs = X_train[idx]
 
-            noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
-            fake_imgs = self.generator.predict(noise)
+                noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
+                fake_imgs = self.generator.predict(noise)
 
-            d_loss_real = self.discriminator.train_on_batch(real_imgs, real)
-            d_loss_fake = self.discriminator.train_on_batch(fake_imgs, fake)
-            d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+                d_loss_real = self.critic.train_on_batch(real_imgs, real)
+                d_loss_fake = self.critic.train_on_batch(fake_imgs, fake)
+                d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+
+                for layer in self.critic.layers:
+                    weights = layer.get_weights()
+                    weights = [np.clip(weight, -self.clip_value, self.clip_value) for weight in weights]
+                    layer.set_weights(weights)
 
             g_loss = self.combined.train_on_batch(noise, real)
 
-            print("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100*d_loss[1], g_loss))
+            print("%d [D loss: %f] [G loss: %f]" % (epoch, 1 - d_loss[0], 1 - g_loss[0]))
 
             if epoch % save_interval == 0:
                 self.save_imgs(epoch)
@@ -126,7 +139,7 @@ class DCGAN():
         r, c = 5, 5
         noise = np.random.normal(0, 1, (r * c, self.latent_dim))
         gen_imgs = self.generator.predict(noise)
-        gen_imgs = 0.5 * gen_imgs + 0.5
+        gen_imgs = 0.5 * gen_imgs + 1
 
         fig, axs = plt.subplots(r, c)
         cnt = 0
@@ -140,5 +153,5 @@ class DCGAN():
 
 
 if __name__ == '__main__':
-    dcgan = DCGAN()
-    dcgan.train(epochs=30000, batch_size=32, save_interval=200)
+    wgan = WGAN()
+    wgan.train(epochs=30000, batch_size=64, save_interval=200)
